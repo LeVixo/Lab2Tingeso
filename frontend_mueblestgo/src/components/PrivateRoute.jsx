105801b7e10898a91ae304eb27ee8292d4ee860b
LeVixo <91557877+LeVixo@users.noreply.github.com>
2022-11-23 01:57:06 -0300

    Revert " [feat] to new repo"
    This reverts commit 70bba6470d71f50596ab09906965b158508ce5c0.

--- frontend_mueblestgo/src/components/PrivateRoute.jsx
+++
@@ -1,6 +0,0 @@
-import { useKeycloak } from '@react-keycloak/web'
-
-export default function PrivateRoute({ children }) {
-  const { keycloak } = useKeycloak()
-  return !keycloak.authenticated ? setTimeout(() => keycloak.login(), 1)  :  children;
-}
