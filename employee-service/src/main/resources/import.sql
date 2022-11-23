5cee8f40e1ddd69c39274ad72974e86366940c17
LeVixo <91557877+LeVixo@users.noreply.github.com>
2022-11-23 02:08:55 -0300

    [feat]: dummy

--- employee-service/src/main/resources/import.sql
+++ employee-service/src/main/resources/import.sql
@@ -1,3 +1,3 @@
-INSERT INTO employee(rut, `name`,last_name,category,birth_date,entry_date) VALUES("20457671-9","Rhaenyra","Targaryen","A","1955/03/22","2010/05/24");
-INSERT INTO employee(rut, `name`,last_name,category,birth_date,entry_date) VALUES("27752982-4","Otto","HighTower","B","2001/11/18","2015/05/10");
-INSERT INTO employee(rut, `name`,last_name,category,birth_date,entry_date) VALUES("25752982-4","Laris","Strong","C","1995/11/11","2017/05/10");
+INSERT INTO employee(rut, `name`,last_name,category,birth_date,entry_date) VALUES("1-9","Juan","Euclevio","A","1960/02/01","2010/04/29");
+INSERT INTO employee(rut, `name`,last_name,category,birth_date,entry_date) VALUES("2-9","Pedro","ramon","B","1980/02/01","2020/04/29");
+INSERT INTO employee(rut, `name`,last_name,category,birth_date,entry_date) VALUES("3-9","Maria","Euclevio","C","1990/02/01","2015/04/29");
